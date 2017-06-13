d85a9c8e9d9fe8e92d4a08845db4428bb85ad498
hc <chrislue1986@hotmail.com>
2017-06-13 19:26:57 +0800

    compile test

--- xgame.cpp
+++ xgame.cpp
@@ -2,18 +2,35 @@
 #include <stdlib.h>
 
 #include "list.h"
-#include "pattern.h"
-
-class KG_Example// : private xzero::KG_UnConstructable
-{
-    KG_SINGLETON_DCL_STRICT(KG_Example)
-};
 
 int main()
 {
-    KG_InterlockedListHead h;
-    KG_Example &r = KG_SINGLETON_REF(KG_Example);
-    KG_Example *p = KG_SINGLETON_PTR(KG_Example);
+    xzero::PKG_InterlockedListHead pHead = xzero::KG_InterlockedList::Initialize();
+
+    for (auto i = 0; i < 10; i++)
+    {
+        xzero::PKG_InterlockedListNode pNode = new xzero::KG_InterlockedListNode();
+        xzero::KG_InterlockedList::KG_PushNode(pHead, pNode);
+    }
+    
+    printf("%d, %d\n", pHead->m_nLength, pHead->m_nOpTimes);
+
+    for (auto i = 0; i < 5; i++)
+    {
+        auto p = xzero::KG_InterlockedList::KG_PopNode(pHead);
+        delete p;
+    }
+
+    printf("%d, %d\n", pHead->m_nLength, pHead->m_nOpTimes);
+
+    for (auto i = 0; i < 5; i++)
+    {
+        auto p = xzero::KG_InterlockedList::KG_PopNode(pHead);
+        delete p;
+    }
+
+    printf("%d, %d\n", pHead->m_nLength, pHead->m_nOpTimes);
+
     return 0;
 }
 
