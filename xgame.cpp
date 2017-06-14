33cc79ccbf1ea7c795f9a97e9f1712a1acf4323e
hc <chrislue1986@hotmail.com>
2017-06-14 17:48:44 +0800

    nothing new

--- xgame.cpp
+++ xgame.cpp
@@ -1,35 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "list.h"
+#include "mempool.h"
 using namespace xzero;
 
 int main()
 {
-    PKG_InterlockedListHead pHead = KG_InitializeInterlockedList();
-
-    for (auto i = 0; i < 10; i++)
-    {
-        KG_PushNodeToInterlockedList(pHead, new KG_InterlockedListNode());
-    }
-    
-    printf("%d, %d\n", pHead->m_nLength, pHead->m_nOpTimes);
-
-    for (auto i = 0; i < 5; i++)
-    {
-        auto p = KG_PopNodeFromInterlockedList(pHead);
-        delete p;
-    }
-
-    printf("%d, %d\n", pHead->m_nLength, pHead->m_nOpTimes);
-
-    for (auto i = 0; i < 5; i++)
-    {
-        auto p = KG_PopNodeFromInterlockedList(pHead);
-        delete p;
-    }
-
-    printf("%d, %d\n", pHead->m_nLength, pHead->m_nOpTimes);
-
+    printf("%d", sizeof(KG_MemBlock));
     return 0;
 }
